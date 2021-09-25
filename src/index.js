910e85d888b42a7956378222ce4997fae9c2d499
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2021-09-25 14:52:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -229,7 +229,7 @@ async function forceConfirm(side, oldPrice) {
 
 const checkBalances = async () => {
   balances = await bc.balance().catch(e => console.log(e));
-  const { BRL, BTC } = balances;
+  const { BRL = {}, BTC = {} } = balances;
   let priceBTC = await bc.ticker();
 
   await bot.telegram.sendMessage(botchat,
